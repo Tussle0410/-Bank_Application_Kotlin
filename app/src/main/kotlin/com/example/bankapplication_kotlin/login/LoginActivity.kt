19c93c70c2988f91fbc29fdcc1f429d7718d189d
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-02-21 22:48:40 +0900

    Splash update

---
+++ app/src/main/kotlin/com/example/bankapplication_kotlin/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bankapplication_kotlin.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bankapplication_kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
---
+++ app/src/main/kotlin/com/example/bankapplication_kotlin/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bankapplication_kotlin.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bankapplication_kotlin.login.LoginActivity
+import com.example.bankapplication_kotlin.R
+import org.jetbrains.anko.startActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    companion object{
+        private const val splash_Time_OUT : Long = 3000
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_page)
+
+        Handler().postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        }, splash_Time_OUT)
+    }
+}
