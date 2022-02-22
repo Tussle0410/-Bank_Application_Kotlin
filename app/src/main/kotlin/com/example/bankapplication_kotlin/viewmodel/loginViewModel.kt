fd8630a87f9c1646b2fcac1af306ac9333e1d9b7
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-02-22 20:00:30 +0900

    Splash update

---
+++ app/src/main/kotlin/com/example/bankapplication_kotlin/viewmodel/loginViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bankapplication_kotlin.viewmodel
+
+import android.app.Activity
+import androidx.lifecycle.ViewModel
+
+class loginViewModel :ViewModel() {
+    private lateinit var activity : Activity
+}
