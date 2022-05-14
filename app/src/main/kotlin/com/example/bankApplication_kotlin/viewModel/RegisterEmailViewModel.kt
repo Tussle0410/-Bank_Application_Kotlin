779ed3c7012aae2bb81d9c47e5be6ec77bab17f7
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-05-15 06:57:43 +0900

    retrofit2 checkEmail add

--- app/src/main/kotlin/com/example/bankApplication_kotlin/viewModel/RegisterEmailViewModel.kt
+++ app/src/main/kotlin/com/example/bankApplication_kotlin/viewModel/RegisterEmailViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.bankApplication_kotlin.viewModel
 
 import android.app.Application
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.AdapterView
@@ -8,8 +9,12 @@ import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.bankApplication_kotlin.api.RegisterAPI
 import com.example.bankApplication_kotlin.event.Event
 import com.example.bankApplication_kotlin.sharedPreference.PreferenceApplication
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -60,8 +65,23 @@ class RegisterEmailViewModel(application: Application) : AndroidViewModel(applic
         val email = emailID.value + "@" + address.value
         val matcher = pattern.matcher(email)
         if(matcher.find()){
-            PreferenceApplication.prefs.registerSetString("userEmail",email)
-            _nextEvent.value = Event(true)
+            val api = RegisterAPI.create()
+            api.regCheckEmail(email).enqueue(object : Callback<String>{
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    if(response.isSuccessful) {
+                        if (response.body() == "true") {
+                            PreferenceApplication.prefs.registerSetString("Email", email)
+                            _nextEvent.value = Event(true)
+                        } else {
+                            Toast.makeText(mApplication, "이미 등록된 이메일입니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.d("CheckEmailFail",t.message.toString())
+                }
+            })
         }
         else
             Toast.makeText(mApplication,"이메일 형식을 확인해주시기 바랍니다.",Toast.LENGTH_SHORT).show()
