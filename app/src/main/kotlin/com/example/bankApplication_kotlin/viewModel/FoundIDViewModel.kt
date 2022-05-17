2c27118ea1df7121d6c16dad015169a17c0a44df
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-05-18 07:07:11 +0900

    FoundID API,Activity....etc..

---
+++ app/src/main/kotlin/com/example/bankApplication_kotlin/viewModel/FoundIDViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bankApplication_kotlin.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FoundIDViewModel(application: Application) : AndroidViewModel(application) {
+
+}
