b9fc59ac503aa622bb7c1d1f52a210440325e449
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-05-20 05:50:44 +0900

    Preference Init Add

--- app/src/main/kotlin/com/example/bankApplication_kotlin/sharedPreference/PreferenceUtil.kt
+++ app/src/main/kotlin/com/example/bankApplication_kotlin/sharedPreference/PreferenceUtil.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 class PreferenceUtil(context : Context) {
     //내부 저장 데이터
     private val loginPrefs : SharedPreferences = context.getSharedPreferences("login",0)
+    private val userPrefs : SharedPreferences = context.getSharedPreferences("user",0)
     private val registerPrefs : SharedPreferences = context.getSharedPreferences("register",0)
     //로그인 관련 내부 저장소 저장 및 반환
     fun loginGetString(key : String,value:String):String
@@ -30,5 +31,15 @@ class PreferenceUtil(context : Context) {
     //회원가입 관련 내부 저장소 초기화
     fun registerInit(){
         registerPrefs.edit().clear().apply()
+    }
+    //로그인한 유저 관련 내부 저장소 저장 및 반환
+    fun userGetString(key: String,value: String):String
+        =userPrefs.getString(key,value).toString()
+    fun userSetString(key: String, str:String){
+        userPrefs.edit().putString(key,str).apply()
+    }
+    //로그인한 유저 관련 내부 저장소 초기화
+    fun userInit(){
+        userPrefs.edit().clear().apply()
     }
 }
